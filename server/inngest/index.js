84eaa51654f0817664af091f0927355ce250ddbf
Vanshshah13 <shahvansh848@gmail.com>
2026-02-24 18:18:55 +0530

    Fix prisma config

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,7 @@
 import {Inngest} from "inngest"
 import prisma from "../configs/prisma.js"
 
-export const inngest = new inngest({id : "project-management"})
+export const inngest = new Inngest({id : "project-management"})
 
 const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
@@ -25,11 +25,9 @@ const syncUserDeletion = inngest.createFunction(
     async ({event}) => {
         const { data} = event
         await prisma.user.delete({
-            data : {
                 where : {
                     id : data.id,
                 }
-            }
         })
     }
 )
@@ -44,7 +42,7 @@ const syncUserUpdation = inngest.createFunction(
                 id : data.id
             },
             data : {
-                email : data.email_addresses[0]?.email_addres,
+                email : data.email_addresses[0]?.email_address,
                 name : data.first_name + " " + data.last_name,
                 image : data?.image_url,
             }
